2e2a1de276d0575a0795e96ab3ef2a37d87d0dab
igor.rocha <igor.rocha@vidadeumsysadmin.me>
2021-09-23 14:32:15 -0300

    changes for frontend service

--- Kubernetes/App/index.js
+++ Kubernetes/App/index.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const app = express()
+const morgan = require('morgan')
+
+app.use(morgan("combined"))
 
 app.get('/', (req, res) =>{
     res.send("Welcome to main page.")
+})
+
+app.get('/frontend', (req, res) =>{
+    res.sendFile(__dirname + "/index.html")
 })
 
 app.listen(3000, () =>{console.log("Server up.")})
